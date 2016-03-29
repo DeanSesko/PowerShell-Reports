ddf91a101e295a9801e030d5359f2f37e00ab15f
Dean Sesko <DeanSesko@hotmail.com>
2016-03-29 17:16:31 -0400

    Code Update

---
+++ SMTP Stats.ps1
@@ -0,0 +1,71 @@
+$end = Get-Date
+$start = $end.AddDays(-5)
+
+$days = "from: $start to:$end"     
+
+
+
+$InternalNum = 0
+$InternalSize = 0
+
+$ExternalSentNum = 0
+$ExternalSentSize = 0
+
+$ExternalRecNum = 0
+$ExternalRecSize = 0
+
+$ServerName = $args[0]
+
+$DomainHash = @{}
+$msgIDArray = @{}
+
+
+get-accepteddomain | ForEach-Object{
+if ($_.DomainType -eq "Authoritative"){
+$DomainHash.add($_.DomainName.SmtpDomain.ToString().ToLower(),1)
+}
+
+}
+
+Get-MessageTrackingLog -Server $ServerName -ResultSize Unlimited -Start $start -End $end | ForEach-Object{
+if ($_.EventID.ToString() -eq "SEND" -bor $_.EventID.ToString() -eq "RECEIVE"){
+foreach($recp in $_.recipients){
+if($recp.ToString() -ne ""){
+$unkey = $recp.ToString() + $_.Sender.ToString() + $_.MessageId.ToString()
+if ($msgIDArray.ContainsKey($unkey) -eq $false){
+$msgIDArray.Add($unkey,1)
+$recparray = $recp.split("@")
+$sndArray = $_.Sender.split("@")
+if ($_.Sender -ne ""){
+if ($DomainHash.ContainsKey($recparray[1])){
+if ($DomainHash.ContainsKey($sndArray[1])){
+$InternalNum = $InternalNum + 1
+$InternalSize = $InternalSize + $_.TotalBytes/1024
+}
+else{
+
+$ExternalRecNum = $ExternalRecNum + 1 
+$ExternalRecSize = $ExternalRecSize + $_.TotalBytes/1024
+} 
+}
+else{
+if ($DomainHash.ContainsKey($sndArray[1])){
+$ExternalSentNum = $ExternalSentNum + 1 
+$ExternalSentSize = $ExternalSentSize + $_.TotalBytes/1024 
+} 
+}
+}
+}
+
+}
+} 
+}
+
+}
+
+"Sent/Recieved Internally Number : " + $InternalNum
+"Sent/Recieved Internally Size : " + [math]::round($InternalSize,2)
+"Externally Sent Number : " + $ExternalSentNum
+"Externally Sent Size : " + [math]::round($ExternalSentSize,2)
+"Externally Recieved Number : " + $ExternalRecNum
+"Externally Recieved Size : " + [math]::round($ExternalRecSize,2)
---
+++ SMTPStatsReport.TempPoint.ps1
@@ -0,0 +1,66 @@
+﻿$Localhost = $env:COMPUTERNAME
+#Load Exchange PS Snapin
+
+Write-Host "running..."
+
+$hubs = Get-TransportServer
+
+# Get the start date for the tracking log search
+$End = Get-Date
+$Start = $End.AddDays(-5)
+
+# Get the end date for the tracking log search
+
+
+$Datum = $Start.ToShortDateString()
+
+$receive = $hubs | get-messagetrackinglog -Start $Start -End $End -EventID "RECEIVE" -ResultSize Unlimited | select Sender, RecipientCount, TotalBytes, Recipients
+$send = $hubs | get-messagetrackinglog -Start $Start -End $End -EventID "SEND" -ResultSize Unlimited | select Sender, RecipientCount, TotalBytes
+$mreceive = $receive | Measure-Object TotalBytes -maximum -minimum -average -sum
+$msend = $send | Measure-Object TotalBytes -maximum -minimum -average -sum
+
+$anzahl = $mreceive.count + $msend.count
+$volumen = ($mreceive.sum + $msend.sum) / (1024 * 1024)
+
+$volumen = "{0:N2}" -f $volumen + " MB"
+
+$msendmb = $msend.sum / (1024 * 1024)
+$vsend = "{0:N2}" -f $msendmb + " MB"
+$bigsend = $msend.maximum / (1024 * 1024)
+$avsend = $msend.average / 1024
+
+$bigsendmb = "{0:N2}" -f $bigsend + " MB"
+$avsendkb = "{0:N2}" -f $avsend + " KB"
+
+$mreceivemb = $mreceive.sum / (1024 * 1024)
+$vreceive = "{0:N2}" -f $mreceivemb + " MB"
+$bigreceive = $mreceive.maximum / (1024 * 1024)
+$avreceive = $mreceive.average / 1024
+
+$bigreceivemb = "{0:N2}" -f $bigreceive + " MB"
+$avreceivekb = "{0:N2}" -f $avreceive + " KB"
+
+$computer = gc env:computername
+$obj = new-object psObject
+
+$obj | Add-Member -MemberType noteproperty -Name "Generated on server:" -Value $Computer
+$obj | Add-Member -MemberType noteproperty -Name "Date :" -Value $Datum
+$obj | Add-Member -MemberType noteproperty -Name "Sent mails :" -Value $msend.Count
+$obj | Add-Member -MemberType noteproperty -Name "Size of sent mails:" -Value $vsend
+$obj | Add-Member -MemberType noteproperty -Name "Size of biggest mail out:" -value $bigsendmb
+$obj | Add-Member -MemberType noteproperty -Name "Average size out :" -value $avsendkb
+$obj | Add-Member -MemberType noteproperty -Name "Quantity incoming mails :" -Value $mreceive.Count
+$obj | Add-Member -MemberType noteproperty -Name "Size of received mails :" -Value $vreceive
+$obj | Add-Member -MemberType noteproperty -Name "Size of biggest mail in :" -value $bigreceivemb
+$obj | Add-Member -MemberType noteproperty -Name "Average size in :" -value $avreceivekb
+$obj | Add-Member -MemberType noteproperty -Name "Overall quantity :" -Value $anzahl
+$obj | Add-Member -MemberType noteproperty -Name "Overall size :" -Value $volumen
+
+$out = $Datum + ";" + $msend.count + ";" + $vsend + ";" + $mreceive.count + ";" + $vreceive + ";" + $anzahl + ";" + $volumen
+$out | out-file c:\scripts\daily.csv -append -encoding default
+
+$obj = $obj -replace ("@{", "")
+$obj = $obj -replace ("=", ":`t")
+$obj = $obj -replace ("; ", "`n")
+$obj = $obj -replace ("}", "`n")
+$obj | out-file C:\Scripts\SMTPStats.txt
